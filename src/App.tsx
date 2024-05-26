20703603737a50557939ca96e64ef5524b8e4bde
Farrux <farrux.avganov@mail.ru>
2024-05-27 03:59:36 +0500

    start responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Dashboard } from './components';
 
 const App: FC = () => {
   return (
-    <div className="max-w-[1600px] min-h-[1200px] bg-gray_1 m-auto mt-5">
+    <div className="max-w-[1600px] min-h-[1200px] bg-gray_1 m-auto mt-5 xl:max-w-[1279px]">
       <Dashboard />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,19 @@ export default  withMT({
 			cardIcon4: "#24D6A5"
 		},
 	},
+	screens: {
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
   },
   plugins: [],
 })
